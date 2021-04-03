0ccc36c450b014f56a61a3537522a14246d6f8c6
SamNorman7103 <70028250+SamNorman7103@users.noreply.github.com>
2021-04-03 11:50:57 -0600

    moved app-routing to modules folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseSelectComponent } from './components/course-select/course-select.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
