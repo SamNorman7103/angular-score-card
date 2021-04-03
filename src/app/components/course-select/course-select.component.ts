f857bb78e8f41729e49c27ef2b17930517b3aee9
SamNorman7103 <70028250+SamNorman7103@users.noreply.github.com>
2021-04-03 13:29:01 -0600

    added a gimic table for course selection

--- src/app/components/course-select/course-select.component.ts
+++ src/app/components/course-select/course-select.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CoursesService } from '../../services/courses.service'
+import { Course } from 'src/app/interfaces/course'
 @Component({
   selector: 'course-select',
   templateUrl: './course-select.component.html',
   styleUrls: ['./course-select.component.scss']
 })
 export class CourseSelectComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'image']
+  courses: Course[];
 
-  constructor() { }
+  constructor(
+    private CoursesService: CoursesService,
+  ) { 
+    
+  }
 
   ngOnInit(): void {
+    this.courses = this.CoursesService.courses
   }
 
 }
